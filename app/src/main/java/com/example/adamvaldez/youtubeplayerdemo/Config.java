2e1a36b586ff225bb61956c82ed36c6bcf081367
adamvaldez <adamxvaldez@gmail.com>
2015-03-05 13:54:34 -0800

    Initial commit

---
+++ app/src/main/java/com/example/adamvaldez/youtubeplayerdemo/Config.java
@@ -0,0 +1,20 @@
+package com.example.adamvaldez.youtubeplayerdemo;
+
+/**
+ * Created by adamvaldez on 3/3/15.
+ */
+public class Config
+{
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyB1JFUOlPNcjDLLubHzB11OYC9ZLjatD30";
+
+    // YouTube video id for Armbar
+    public static final String YOUTUBE_VIDEO_CODE = "ytQHEuSV3JA";
+
+    // YouTube video id for Guillotine
+    public static final String YOUTUBE_VIDEO_CODE1 = "3qJhvFVyUqQ";
+
+    // YouTube video id for Throw
+    public static final String YOUTUBE_VIDEO_CODE2 = "W0xW8X_Y35k";
+}
